d15c20df997c1bf63a2c7fd7bed20c82314a8847
Rahul <rajpurohit.lp@gmail.com>
2023-10-05 16:22:53 +0530

    first commit for the website

---
+++ javascript/main.js
@@ -0,0 +1,52 @@
+
+// change navbar color on scroll
+const navBar = document.querySelector('.navbar');
+const work_section = document.querySelector('.work_section');
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY >= (work_section.offsetTop - 56)){
+        navBar.classList.add('navbar-dark')
+        navBar.classList.add('navbar_scrolled')
+    } else {
+        navBar.classList.remove('navbar-dark')
+        navBar.classList.remove('navbar_scrolled')
+    }
+})
+
+// animate_hidden element animations on scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('animate_show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.animate_hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElementsTop = document.querySelectorAll('.animate_hidden_top');
+hiddenElementsTop.forEach((el) => observer.observe(el));
+
+
+// Mouse cursor animations
+const trailer = document.querySelector('.trailer');
+
+document.addEventListener('mousemove', e=>{
+    trailer.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+
+    const interactable = e.target.closest(".animate_hidden"); 
+    const isIntersecting = interactable !== null;
+
+    const keyfrmaes = {
+        transform: `scale(${isIntersecting? 2:1})`,
+        border: `${isIntersecting? 1:2}px solid #267CFF`,
+    }
+
+    trailer.animate(keyfrmaes, {
+        duration: 300,
+        fill:"forwards"
+    });
+})
+
