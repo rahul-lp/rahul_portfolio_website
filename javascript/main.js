e2ad182ad1171f555e51cc5e1fffaaa1df3e56fe
Rahul <rajpurohit.lp@gmail.com>
2023-10-06 15:19:50 +0530

    Added links and new empty pages

--- javascript/main.js
+++ javascript/main.js
@@ -54,7 +54,7 @@ document.addEventListener('mousemove', e=>{
 
 function setPDFHeight() {
     let viewportHeight = window.innerHeight;
-    document.getElementById("pdfEmbed").style.height = `${0.8 * viewportHeight}px`;
+    document.getElementById("pdfEmbed").style.height = `${0.9 * viewportHeight}px`;
 }
 
 window.addEventListener("DOMContentLoaded", setPDFHeight);
